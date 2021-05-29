15eee055117669c82003e427556fc7c2fbfd8ef7
sharath sr <76243046+Sharath-a26@users.noreply.github.com>
2021-05-29 17:08:28 +0530

    Updated changes

--- bot.py
+++ bot.py
@@ -1,31 +1,34 @@
 from telegram import *
 from telegram.ext import *
 import matplotlib.pyplot as plt
-import numpy
-def sub(update,context):
-    update.message.reply_text('Type something')
-    first_name = update.message.from_user
-    update.message.reply_text('hello')
+def start(update,context):
 
+    name = update.message.from_user.first_name
+    update.message.reply_text(f"Hi {name}!")
+def about(update,context):
+    botinfo = bot.get_me()
+    update.message.reply_text(f"{bot.get_me()}")
 bot = Bot("1826479712:AAEJy4VtHb1ZOQEHImoO2xzNRE6dtkwMR00")
 #print(bot.get_me())
 updater = Updater("1826479712:AAEJy4VtHb1ZOQEHImoO2xzNRE6dtkwMR00",use_context=True)
 dispatcher = updater.dispatcher
-#print(dispatcher)
-def test_function(update:Update,context:CallbackContext):
-    bot.send_message(chat_id=update.effective_chat.id,text="tutorial link: https://youtu.be/7qJFtGi0hNQ",)
-start = CommandHandler('motion_detection',test_function)
+
+def welcome(update:Update,context:CallbackContext):
+    bot.send_message(chat_id=update.effective_chat.id,text=f"{sub()}",)
+start = CommandHandler('start',start)
+
 dispatcher.add_handler(start)
+
 updater.start_polling()
 def test_function1(update:Update,context:CallbackContext):
 
-    bot.send_message(chat_id=update.effective_chat.id,text=f"{sub()}",)
-start1 = CommandHandler('start',sub)
+    bot.send_message(chat_id=update.effective_chat.id,text=f"",)
+start1 = CommandHandler('about',about)
 dispatcher.add_handler(start1)
 updater.start_polling()
 def test_function2(update:Update,context:CallbackContext):
 
-    bot.send_message(chat_id=update.effective_chat.id,text=f"{sub()}",)
+    bot.send_message(chat_id=update.effective_chat.id,text=f"",)
 start2 = CommandHandler('ststt',test_function2)
 dispatcher.add_handler(start2)
 updater.start_polling()
